9d95e40539e2d7197a519ef746d3e8be08a423da
Aaron <AaronLev.210@outlook.com>
2026-08-10 18:44:18 +1000

    aspect ratio fixed on video

--- components/PortraitVideo.tsx
+++ components/PortraitVideo.tsx
@@ -29,7 +29,7 @@ export default function PortraitVideo({ src, embedUrl, caption, poster, classNam
             autoPlay
             preload="metadata"
             controls
-            className="absolute inset-0 w-full h-full object-cover"
+            className="absolute inset-0 w-full h-full object-contain"
           />
         ) : (
           <iframe
